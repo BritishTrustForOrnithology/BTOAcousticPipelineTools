ea9d994154c2919125eca675b56932b41ea35476
Simon Gillings <simon.gillings@bto.org>
2023-12-06 23:44:13 +0000

    add check for corrupt files

--- R/audit_audio.R
+++ R/audit_audio.R
@@ -15,6 +15,16 @@ audit_audio <- function(path_to_process, files_old) {
     this_dirname <- dirname(this_wav)
     this_file <- basename(this_wav)
     
+    #check if the file can be read
+    file_corrupt <- tryCatch({
+      # Attempt to read the file
+      data <- tuneR::readWave(this_wav, header=TRUE)
+      0
+    }, error = function(e) {
+      # Handle the error
+      1
+    })
+    
     #check filename compatibility using various filename templates
     pattern_underscore <- "\\d{8}_\\d{6}"
     pattern_hyphen <- "\\d{8}-\\d{6}"
@@ -35,8 +45,10 @@ audit_audio <- function(path_to_process, files_old) {
       filename_fail <- 0
     }
     
-    #check for guano
-    this_guano <- read_guano(this_wav)
+    #check for guano (if the file is not corrupt)
+    if(file_corrupt==0) this_guano <- read_guano(this_wav)
+    if(file_corrupt==1) this_guano <- list()
+    
     has_guano <- FALSE
     dt_guano <- NA
     lat_guano <- NA
@@ -68,6 +80,8 @@ audit_audio <- function(path_to_process, files_old) {
     
     #check for XML
     #is there an XML file?
+    datetime_value <- NA
+    dt <- NA
     file_xml <- gsub(".wav", ".xml", this_wav)
     if(!file.exists(file_xml)) {
       has_xml <- FALSE
@@ -108,15 +122,17 @@ audit_audio <- function(path_to_process, files_old) {
     
     #if the file needs renaming, and can be renamed based on data, create newname
     newname <- NA
+    dt2use <- NA
     if(renamable==1) {
       dt2use <- ifelse(!is.na(dt_guano), dt_guano, dt_xml)
-      newname <- file.path(this_dirname, paste0(dt2use,"_",this_file))
+      newname <- file.path(this_dirname, paste0(dt2use,"_old_",this_file))
     }
     
     
     
     outs[[f]] <- data.frame(original_name = this_wav, 
                             directory = this_dirname, 
+                            file_corrupt = file_corrupt,
                             success = success, 
                             filename_fail,
                             has_guano, 
@@ -129,8 +145,8 @@ audit_audio <- function(path_to_process, files_old) {
                             renamable, 
                             new_name = newname, 
                             unrenamable)
-    rm(list=c('this_wav', 'this_dirname', 'filename_fail', 'success', 'has_guano', 'dt_guano', 'lat_guano', 'lon_guano', 
-              'has_xml', 'dt_xml', 'dt_agree', 'renamable', 'newname', 'dt', 'this_guano', 'datetime_value', 'dt2use', 'unrenamable'))
+    rm(list=c('this_wav', 'this_dirname', 'file_corrupt', 'filename_fail', 'success', 'has_guano', 'dt_guano', 'lat_guano', 'lon_guano', 
+              'has_xml', 'dt_xml', 'dt_mismatch', 'renamable', 'newname', 'dt', 'this_guano', 'datetime_value', 'dt2use', 'unrenamable'))
     
   } #end file iteration loop
   #unpack
@@ -138,11 +154,12 @@ audit_audio <- function(path_to_process, files_old) {
   file_data$nfiles <- 1 #dummy var to count fils
   
   #get summary stats for each folder
-  summary_per_dir <- aggregate(data = file_data, cbind(nfiles, filename_fail, success, has_guano, has_xml, renamable, unrenamable) ~ directory, sum)
+  summary_per_dir <- aggregate(data = file_data, cbind(nfiles, file_corrupt, filename_fail, success, has_guano, has_xml, renamable, unrenamable) ~ directory, sum)
   
   #headline stats
   n_files <- length(files_old)
   n_dirs <- nrow(summary_per_dir)
+  n_files_corrupt <- sum(summary_per_dir$file_corrupt)
   
   #how many of the original files can be processed immediately?
   n_ok_to_process <- sum(file_data$success)
@@ -162,12 +179,15 @@ audit_audio <- function(path_to_process, files_old) {
   #how many can't be renamed?
   n_cannot_rename <- sum(file_data$unrenamable)
   
-  #allow renaming only if no files that can't be renamed, no duplicates will be generated
-  allow_rename <- ifelse(n_cannot_rename == 0 & n_duplicated_newnames == 0, TRUE, FALSE)
+  #allow renaming only if: all filenames fail, no files that can't be renamed, no duplicates will be generated
+  all_filename_fail <- ifelse(sum(file_data$filename_fail) == n_files,1,0)
+  allow_rename <- ifelse(all_filename_fail == 1 & n_cannot_rename == 0 & n_duplicated_newnames == 0, TRUE, FALSE)
   
   output <- list(n_files = n_files,
                  n_dirs = n_dirs,
+                 n_files_corrupt = n_files_corrupt,
                  all_ok = all_ok,
+                 all_filename_fail = all_filename_fail,
                  allow_rename = allow_rename,
                  n_ok_to_process = n_ok_to_process,
                  n_need_renaming = n_need_renaming,
