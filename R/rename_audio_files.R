188d3f012c9f76fcfa5dfbde3cbc19d7c5a4aa03
Simon Gillings <simon.gillings@bto.org>
2023-12-06 23:49:36 +0000

    docs

--- R/rename_audio_files.R
+++ R/rename_audio_files.R
@@ -1,10 +1,15 @@
 #' Rename audio files
 #' 
-#' @description Rename the audio files.
+#' @description Rename the audio files from non-Pipeline friendly version to 
+#' friendly version with date_time format. Save a log file of the changes.
+#' 
+#' @param path_to_process = the directory to audit
+#' @param file_info = a dataframe describing all the files in the batch containing, 
+#' among other things, columns for original_name and new_name
 #' 
 rename_audio_files <- function(path_to_process, file_info) {  
   
-  #pre-pend root path from search
+  #get the old and new names
   files_old <- file_info$original_name
   files_new <- file_info$new_name
   
