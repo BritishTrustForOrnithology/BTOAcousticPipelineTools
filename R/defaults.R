eb7ccd1a0dfc6f0f4f46a82799f2c728412f65e4
Simon Gillings <simon.gillings@bto.org>
2023-12-06 23:44:02 +0000

    updated version number

--- R/defaults.R
+++ R/defaults.R
@@ -9,7 +9,7 @@ passed_path_output <- getShinyOption('path_output', default = NULL)
 
 #constants
 credit <- 'App written by Simon Gillings, BTO'
-version <- read.csv(file.path(dirname(path_to_app),'version.txt'))
+version <- read.csv(file.path(dirname(path_to_app),'version.txt'), colClasses = 'character')
 
 #get the drive letters
 volumes <- shinyFiles::getVolumes()()
