47e3447894622eba849ca8d9981733998217cfe1
Simon Gillings <simon.gillings@bto.org>
2023-12-19 13:14:37 +0000

    update startup instructions

--- launch_shiny_app.R
+++ launch_shiny_app.R
@@ -2,9 +2,10 @@
 #'
 #'  1. Do not run the app.R program directly. Instead use launch_shiny_app.R
 #'  
-#'  2. Edit the path_to_app line to give the location of the app.R program on 
-#'     your computer. Note that you must use forward (/) slashes, e.g.:
-#'     path_to_app <- 'C:/My Documents/app.R'
+#'  2. Edit the path_to_app line to give the location of the folder containing 
+#'     server.R program on your computer. Note that you must use forward (/) 
+#'     slashes and a final dot, e.g.:
+#'     path_to_app <- 'C:/Users/jo.smith/Downloads/BTOAcousticPipelineToolsPUBLIC-main/.'
 #'
 #'  3. For most users you do not need to change the shinyOptions line. But if you
 #'     are processing a very large volume of files (>100,000) it is sometimes 
@@ -15,7 +16,8 @@
 #'
 #'  4. Run all command lines and the app will launch in a browser winder.
 
-#path to where app is saved on your machine. Update if the launch_shiny_app.R is in a different folder to ui.R
+#path to folder where server.R is saved on your machine, e.g. (note final slash and dot) 
+# 'C:/Users/jo.smith/Downloads/BTOAcousticPipelineToolsPUBLIC-main/.'
 path_to_app <- '.'
 
 #run the app
